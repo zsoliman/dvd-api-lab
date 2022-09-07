1e2a3255cf2e335194e8cc1b7a9a58f562f99af2
Zachary Soliman <zachsoliman1@gmail.com>
2022-09-07 10:26:44 -0400

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :movies
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :movies, only: [:index]
+ 
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
 end
